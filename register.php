debe5392bf5358c94855d306f7bd2ac1718085b4
Aashika <khatiwadaaashika12@gmail.com>
2025-08-31 15:15:09 +0545

    dailydiary1

---
+++ register.php
@@ -0,0 +1,28 @@
+<?php
+require "connect.php";
+
+if(isset($_POST['register'])){
+    $username=$_POST['username'];
+    $password=md5($_POST['password']);
+    $email=$_POST['email'];
+
+    $sql="SELECT * FROM users WHERE username=? OR email=?";
+    $stmt= $pdo->prepare($sql);
+    $stmt->execute([$username,$email]);
+
+    if($stmt->rowCount() > 0){
+        echo "<script>
+                alert('Username or Email already exists!');
+                window.location.href='register.html';
+              </script>";
+        exit;
+    }
+$sql="INSERT INTO users(username,email,password) VALUES(?,?,?)";
+$stmt=$pdo->prepare($sql);
+$stmt->execute([$username,$email,$password]);
+
+echo "<script>
+alert('Registration successful');
+ window.location.href='login.html';
+</script>";
+}
